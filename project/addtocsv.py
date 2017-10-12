ba37f0e78132935253bba6c6c4822e50af9e813b
freejoe76 <jmurphy@denverpost.com>
2017-10-12 17:54:54 -0400

    Work on clarity of var names and tests

--- project/addtocsv.py
+++ project/addtocsv.py
@@ -9,10 +9,11 @@ from cStringIO import StringIO
 
 def addtocsv(args):
     """ 
+        >>> addtocsv({'files': ['tests/data.csv', 'tests/project-longform.csv']})
         """
-    if len(args.fns[0]) > 1:
-        new = csv.DictReader(file(args.fns[0][0], 'rb'), encoding='utf-8')
-        current = csv.DictReader(file(args.fns[0][1], 'rb'), encoding='utf-8')
+    if len(args.files[0]) > 1:
+        new = csv.DictReader(file(args.files[0][0], 'rb'), encoding='utf-8')
+        current = csv.DictReader(file(args.files[0][1], 'rb'), encoding='utf-8')
 
         # Loop through each item in the new csv.
         # If the new item isn't in the current, add it.
@@ -40,13 +41,13 @@ def addtocsv(args):
         # Write the current csv
         # First write all the update & additions, and record the id's.
         # Then loop through the existing records and if we haven't already written them, write 'em.
-        with open(args.fns[0][1], 'rb') as csvfile:
+        with open(args.files[0][1], 'rb') as csvfile:
             h = csv.reader(csvfile)
             fieldnames = h.next()
             del h
 
-        with open(args.fns[0][1], 'wb') as csvfile:
-            current = csv.DictReader(file(args.fns[0][1], 'rb'), encoding='utf-8')
+        with open(args.files[0][1], 'wb') as csvfile:
+            current = csv.DictReader(file(args.files[0][1], 'rb'), encoding='utf-8')
             writefile = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writefile.writeheader()
             ids = []
@@ -57,7 +58,8 @@ def addtocsv(args):
 
             for item in current_items:
                 if item['id'] not in ids:
-                    #print "NEW", item['id']
+                    if args.verbose:
+                        print "NEW", item['id']
                     writefile.writerow(item)
 
 def main(args):
@@ -67,7 +69,7 @@ def build_parser(args):
     """ A method to handle argparse.
         >>> args = build_parser(None)
         >>> print args.verbose
-        True
+        False
         """
     parser = argparse.ArgumentParser(usage='$ python addtocsv.py file-new.csv file-existing.csv',
                                      description='''Takes a list of CSVs passed as args.
@@ -75,7 +77,7 @@ def build_parser(args):
                                      epilog='')
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
     parser.add_argument("--test", dest="test", default=False, action="store_true")
-    parser.add_argument("fns", action="append", nargs="*")
+    parser.add_argument("files", action="append", nargs="*")
     return parser.parse_args()
 
 if __name__ == '__main__':
