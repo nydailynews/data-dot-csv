5eb433a1225078bfc3809dbc9105db5a1485e676
freejoe76 <jmurphy@denverpost.com>
2017-10-16 15:57:31 -0400

    Add blank CSVs

--- project/deploy.py
+++ project/deploy.py
@@ -35,7 +35,7 @@ def main(args):
             if 'csv' not in filename:
                 continue
             if 'category' in filename:
-                project = os.path.join(dirname, filename)
+                category = os.path.join(dirname, filename)
             if 'special' in filename:
                 special = os.path.join(dirname, filename)
                 
